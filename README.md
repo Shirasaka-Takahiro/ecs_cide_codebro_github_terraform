05e9d526828d2ef47159ecfd39dfa38306dedd0d
Shirasaka-Takahiro <kenshirou913@gmail.com>
2025-01-25 18:29:35 +0900

    modified

---
+++ README.md
@@ -0,0 +1,34 @@
+README.md
+■Set-Up
+1. Create S3 for tfstate
+2. Generate public and private key
+3. Write resource's variables in terraform.tfvars
+
+■Resources
+<br />
+Network
+<br />
+ECS Fargate
+<br />
+ACM
+<br />
+ALb x 1(HTTP & HTTPS Listener)
+<br />
+Route53
+<br />
+CodeStartConnections(GitHub)
+<br />
+CodeBuild
+<br />
+CodeDeploy
+<br />
+CodePipeline
+<br />
+CloudWatch
+<br />
+
+■Deploy
+1. Move to direstory
+2. terraform init
+3. terraform plan
+4. terraform apply
---
+++ module/codepipeline/codepipeline.tf
@@ -0,0 +1,73 @@
+##Codepipeline
+resource "aws_codepipeline" "pipeline" {
+  name     = "${var.general_config["project"]}-${var.general_config["env"]}-pipeline"
+  role_arn = var.iam_codepipeline_arn
+
+  artifact_store {
+    location = var.bucket_name
+    type     = "S3"
+  }
+
+  ##Source Stage
+  stage {
+    name = "Source"
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        ConnectionArn        = var.codestarconnections_connection_arn
+        FullRepositoryId     = var.full_repositroy_id
+        BranchName           = var.branch_name
+        OutputArtifactFormat = "CODEBUILD_CLONE_REF"
+      }
+    }
+  }
+
+  ##Build Stage
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+      version          = "1"
+      configuration = {
+        ProjectName = var.codebuild_project_name
+      }
+    }
+  }
+
+  ##Deploy Stage
+  stage {
+    name = "Deploy"
+
+    action {
+      name            = "Deploy"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "CodeDeployToECS"
+      input_artifacts = ["build_output"]
+      version         = "1"
+
+      configuration = {
+        ApplicationName                = var.codedeploy_app_name
+        DeploymentGroupName            = var.codedeploy_deployment_group_name
+        TaskDefinitionTemplateArtifact = "build_output"
+        TaskDefinitionTemplatePath     = var.task_definition_template_path
+        AppSpecTemplateArtifact        = "build_output"
+        AppSpecTemplatePath            = var.app_spec_template_path
+        Image1ArtifactName             = "BuildArtifact"
+        Image1ContainerName            = "IMAGE1_NAME"
+      }
+    }
+  }
+}
